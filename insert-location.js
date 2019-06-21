0902c6604cef05900c24b9f9216b181bacc553bb
Angelo <locomain12@gmail.com>
2019-06-21 19:42:41 +0200

    wefwaef

--- insert-location.js
+++ insert-location.js
@@ -1,23 +1,9 @@
-const { PerformanceObserver, performance } = require('perf_hooks');
+const Utils = require('./utils');
 const mongo = require('mongodb');
 const data = [];
 
-/**
- * Prints the time elapsed by func
- *
- * @param func
- * @returns {number}
- */
-const benchmark = func =>{
-    const timeBefore = performance.now();
-    func();
-    const timeAfter = performance.now();
-    const cost = timeAfter-timeBefore;
 
-    console.log(`Action took ${cost} milliseconds`);
-    return cost;
-};
-
+console.log(Utils);
 
 //generate data
 for(let x = 0; x<1000000; x++){
@@ -26,7 +12,7 @@ for(let x = 0; x<1000000; x++){
         latitude: `10${x}.1234`,
         longitude: `156${x}.234`,
         date: new Date()
-    })
+    });
 }
 
 //insert data
@@ -35,9 +21,9 @@ mongo.MongoClient.connect("mongodb://localhost:27017",(error,mongo)=>{
     const db = mongo.db('school');
     const locationCollection = db.collection('location');
     locationCollection.drop();
-    
+
     console.log("inserting locations");
-    benchmark(()=>
+    Utils.benchmark(()=>
         locationCollection.insertMany(data)
     );
 });

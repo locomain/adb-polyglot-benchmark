58401c8dea79a46f2e4b41ceac6f998d5bd6cd2a
Angelo <locomain12@gmail.com>
2019-06-21 19:07:26 +0200

    eureka

--- insert-location.js
+++ insert-location.js
@@ -1,5 +1,17 @@
 const { PerformanceObserver, performance } = require('perf_hooks');
 const mongo = require('mongodb');
+const data = [];
+
+//generate data
+for(let x = 0; x<1000000; x++){
+    data.push({
+        email: `inserttest${x}@mail.com`,
+        latitude: `10${x}.1234`,
+        longitude: `156${x}.234`,
+        date: new Date()
+    })
+}
+
 
 /**
  *
@@ -16,23 +28,11 @@ const benchmark = func =>{
     return cost;
 };
 
-/**
- *
- */
-const insertLocationTest = db=>{
-    let x = 0;
-    while(x < 10000)
-        db.collection('location').insertOne({
-            email: `inserttest${x}@mail.com`,
-            latitude: `10${x}.1234`,
-            longitude: `156${x}.234`,
-            date: new Date()
-        },(error,response)=>x++)
-};
 
-
-mongo.MongoClient.connect("mongodb://localhost:27017/school",(error,db)=>{
+mongo.MongoClient.connect("mongodb://localhost:27017",(error,db)=>{
     if(error)throw error;
     console.log("inserting locations");
-    benchmark(()=>insertLocationTest(db));
+    benchmark(()=>
+        db.db('school').collection('location').insertMany(data)
+    );
 });

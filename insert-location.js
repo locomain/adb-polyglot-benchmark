5e32fcde5971416b31a8fb86a05be3ecce9670ce
Angelo <locomain12@gmail.com>
2019-06-21 19:13:31 +0200

    insert locations works yo 2018.3514000009745 milliseconds

--- insert-location.js
+++ insert-location.js
@@ -2,18 +2,8 @@ const { PerformanceObserver, performance } = require('perf_hooks');
 const mongo = require('mongodb');
 const data = [];
 
-//generate data
-for(let x = 0; x<1000000; x++){
-    data.push({
-        email: `inserttest${x}@mail.com`,
-        latitude: `10${x}.1234`,
-        longitude: `156${x}.234`,
-        date: new Date()
-    })
-}
-
-
 /**
+ * Prints the time elapsed by func
  *
  * @param func
  * @returns {number}
@@ -29,10 +19,25 @@ const benchmark = func =>{
 };
 
 
-mongo.MongoClient.connect("mongodb://localhost:27017",(error,db)=>{
+//generate data
+for(let x = 0; x<1000000; x++){
+    data.push({
+        email: `inserttest${x}@mail.com`,
+        latitude: `10${x}.1234`,
+        longitude: `156${x}.234`,
+        date: new Date()
+    })
+}
+
+//insert data
+mongo.MongoClient.connect("mongodb://localhost:27017",(error,mongo)=>{
     if(error)throw error;
+    const db = mongo.db('school');
+    const locationCollection = db.collection('location');
+    locationCollection.drop();
+    
     console.log("inserting locations");
     benchmark(()=>
-        db.db('school').collection('location').insertMany(data)
+        locationCollection.insertMany(data)
     );
 });

526c9362b94e00899c3563ff25b177447ce84b37
Angelo <locomain12@gmail.com>
2019-06-23 20:39:41 +0200

    eagaer

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
             host: "localhost",
             user: "root",
             password: "",
-            database:"onderzoek_polyglot"
+            database:"onderzoek_polyglot_new"
         }
     },
     MONGODB: {

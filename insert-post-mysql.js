1ff2b04ab5e13c952af72e1ec2d1e47ba12b5257
Angelo <locomain12@gmail.com>
2019-06-23 14:44:17 +0200

    zefszfezsf

--- insert-post-mysql.js
+++ insert-post-mysql.js
@@ -2,9 +2,9 @@ const Utils = require('./helpers/utils');
 
 (async ()=>{
     await Utils.benchmark(()=>{
-        for(let x = 0; x<50000; x++){ //50000
+        for(let x = 0; x<50000; x++){
             Utils.mysql(`REPLACE INTO post(post_id, location_id, email, title, description, date) 
-                        VALUES ('1111111111${x}', '11111111${x}', 'mail${x}', 'title{$x}', 'degegefz${x}', '0000-00-00')`
+                        VALUES ('111111${x}', '111111${x}', 'mail${x}', 'title{$x}', 'degegefz${x}', '0000-00-00')`
             )
         }
     });
